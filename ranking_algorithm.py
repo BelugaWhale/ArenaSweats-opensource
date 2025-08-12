51358aa97304e1ec3c4993dc7c2e923ddcd7e65c
Ahmed <amunshey@gmail.com>
2025-08-12 22:04:45 +1000

    Try tuned TM 2

--- ranking_algorithm.py
+++ ranking_algorithm.py
@@ -73,7 +73,7 @@ def instantiate_rating_model():
     CURRENTLY ALL PARAMETERS ARE SET TO DEFAULT
     """
     # This instantiation creates a model for games with strict rankings (no draws).
-    return ThurstoneMostellerFull(beta = (25/6) * 100, tau = (25/300) * 100)
+    return ThurstoneMostellerFull(beta=25.0 / 3, tau=25.0 / 100, limit_sigma=True)
 
 def process_game_ratings(model, players, game_id, player_ratings, logger):
     """
