7f232f39e1d7af4d739780dda3438052938acbcb
Ahmed <amunshey@gmail.com>
2025-12-26 15:29:32 +1100

    return beta, change sigma

--- ranking_algorithm.py
+++ ranking_algorithm.py
@@ -108,7 +108,7 @@ def instantiate_rating_model():
     - https://openskill.me/en/stable/models/openskill.models.weng_lin.thurstone_mosteller_full.html
     """
     # This instantiation creates a model for games with strict rankings (no draws).
-    model = ThurstoneMostellerFull(sigma=(25/6), beta=(25/6) * 2.5, tau=(25/300) * 3)
+    model = ThurstoneMostellerFull(sigma=(25/6.5), beta=(25/6) * 4, tau=(25/300) * 1.75)
 
     return model
 
